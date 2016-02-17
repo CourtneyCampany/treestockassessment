5a2e8c5db1e41d538f65bdc8b0e566afaa6181ad
Courtney Campany <courtneycampany@gmail.com>
2016-02-17 16:51:55 +1100

    added to review

--- findings_review.R
+++ findings_review.R
@@ -1,2 +1,7 @@
 ###keep track of specific data findings for tree stock balance
 
+
+
+1. Dumrose_2013.  RCD, Root biomass and volume, R:S by container size
+
+2. Patterson 1996 - RCD, height, mass by container size
