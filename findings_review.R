bf4d77ab67ab4c06be343fa2b5102186257272fd
Courtney Campany <courtneycampany@gmail.com>
2016-02-22 15:44:51 +1100

    added to review

--- findings_review.R
+++ findings_review.R
@@ -5,3 +5,5 @@
 1. Dumrose_2013.  RCD, Root biomass and volume, R:S by container size
 
 2. Patterson 1996 - RCD, height, mass by container size
+
+3. Heishanen_1998 - R:S by soil media (scots pine, silver birch)
