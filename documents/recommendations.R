d49833c971df092ce2ac0839057add0999ba6393
Courtney Campany <courtneycampany@gmail.com>
2016-04-15 16:23:34 +1000

    worked on review

---
+++ documents/recommendations.R
@@ -0,0 +1,5 @@
+##suggestions from other standards
+
+#1. size of container must be specified in addition to size of plant
+#2. plants must be in containers for a minimun of 3 months
+#3. age must be stated
