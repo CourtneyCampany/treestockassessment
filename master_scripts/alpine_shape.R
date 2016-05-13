6c57d49d939930ba8dccb21c77b219477a930911
Courtney Campany <courtneycampany@gmail.com>
2016-05-13 16:38:41 +1000

    added new alpine leaf scripts
    started genereal data functions for leaf data

---
+++ master_scripts/alpine_shape.R
@@ -0,0 +1,33 @@
+source("functions/functions.R")
+
+##Shape parameters for alpine nursery
+
+shape <- read.csv("data/alpine_shape.csv")
+branch <- read.csv("data/alpine_branch.csv")
+
+
+##function to calculate shape parameters
+treeshape_func <- function(x){
+  x$branchgroup <- ifelse(x$branchper30 <= 4, "low",
+                      ifelse(x$branchper30 >=5 & x$branchper30 <= 10, "medium",
+                        "high"))
+  print("successfully grouped branchiness")
+  
+  x$crownlength <- with(x, canopy_top - canopy_bottom)
+  print("successfully calculated crown size")
+return(x)
+}
+
+alpine_shape <- treeshape_func(shape)
+
+alpine_shape_agg <- doBy::summaryBy(maxbranchradius+branchper30+crownlength+canopy_top ~ volume+species, 
+                                    data=alpine_shape, FUN=c(mean,se))
+
+##preliminary plots
+
+plot(crownlength.mean ~ maxbranchradius.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,600), xlim=c(0,200))
+plot(crownlength.mean ~ branchper30.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,600), xlim=c(0,20))
+plot(canopy_top.mean ~ branchper30.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,800), xlim=c(0,20))
+plot(canopy_top.mean ~ crownlength.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,700), xlim=c(0, 600))
+plot(canopy_top.mean ~ maxbranchradius.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,700), xlim=c(0, 200))
+
