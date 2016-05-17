35da45a63bec680ff377af72b1050917cf6b3757
Courtney Campany <courtneycampany@gmail.com>
2016-05-17 17:19:08 +1000

    fixed bad data, made some prelim plots of canopy shape

--- master_scripts/alpine_shape.R
+++ master_scripts/alpine_shape.R
@@ -3,6 +3,8 @@ source("functions/functions.R")
 ##Shape parameters for alpine nursery
 
 shape <- read.csv("data/alpine_shape.csv")
+shape$crownlength <- with(shape, canopy_top - canopy_bottom)
+
 branch <- read.csv("data/alpine_branch.csv")
 alpine_species <- read.csv("data/alpine_species.csv", stringsAsFactors = FALSE)
 
@@ -15,22 +17,29 @@ treeshape_func <- function(x){
   print("successfully grouped branchiness")
   
   x$crownlength <- with(x, canopy_top - canopy_bottom)
+  # if(x$crownlength <= 0) stop("there is bad data because crownlength must be positive")
   print("successfully calculated crown size")
-  x$crowncone <- with(x, (pi*maxbranchradius)*(maxbranchradius+sqrt((crownlength)^2+(maxbranchradius)^2))) #cm for lengths
+  
+  x$crownvolume <- with(x, (pi* (maxbranchradius)^2 * (crownlength/3))/1000000) #units in cm
   print("crown area calculated successfully")
+  
 return(x)
 }
 
 alpine_shape <- treeshape_func(shape)
 
-alpine_shape_agg <- doBy::summaryBy(maxbranchradius+branchper30+crownlength+canopy_top+crowncone ~ volume+species, 
-                                    data=alpine_shape, FUN=c(mean,se))
+alpine_shape_agg <- doBy::summaryBy(maxbranchradius+branchper30+crownlength+canopy_top+crownvolume ~ volume+species, 
+                                    data=alpine_shape, FUN=c(mean), keep.names = TRUE)
 
 
 ###add species information
-test <- merge(alpine_shape, alpine_species, by="species", all=TRUE)
+alpine_trees <- merge(alpine_shape_agg, alpine_species, by="species", all=TRUE)
 
+alpine_shape$crownratio <- with(alpine_shape, crownlength/canopy_top)
+alpine_shape$crownratio2 <- with(alpine_shape, (maxbranchradius*2)/crownlength)
+alpine_shape$crownratio4 <- with(alpine_shape, crownvolume/crownlength)
 
+write.csv(alpine_trees, "calculated_data/alpine_trees.csv", row.names = FALSE)
 
 
 
@@ -41,8 +50,17 @@ plot(crownlength.mean ~ branchper30.mean, data=alpine_shape_agg, col=as.factor(s
 plot(canopy_top.mean ~ branchper30.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,800), xlim=c(0,20))
 plot(canopy_top.mean ~ crownlength.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,700), xlim=c(0, 600))
 plot(canopy_top.mean ~ maxbranchradius.mean, data=alpine_shape_agg, col=as.factor(species), pch=16, ylim=c(0,700), xlim=c(0, 200))
-plot(crowncone.mean ~ maxbranchradius.mean, data=alpine_shape_agg, col=as.factor(species), pch=16)
+plot(crownvolume.mean ~ maxbranchradius.mean, data=alpine_shape_agg, col=as.factor(species), pch=16)
 
-plot(crowncone.mean ~ volume, data=alpine_shape_agg, col=as.factor(species), pch=16)
+plot(crownvolume ~ volume, data=alpine_shape_agg, col=as.factor(species), pch=16)
 plot(branchper30.mean ~ volume, data=alpine_shape_agg, col=as.factor(species), pch=16)
 plot(maxbranchradius.mean ~ volume, data=alpine_shape_agg, col=as.factor(species), pch=16)
+
+plot(lengthtobranchiness ~ volume, data=alpine_trees, col=as.factor(species), pch=16)
+
+par(mar=c(9,5,1,1))
+boxplot(branchper30 ~ species, data=alpine_shape, las=2)
+boxplot(crownratio ~ species, data=alpine_shape, las=2)
+boxplot(crownratio2 ~ species, data=alpine_shape, las=2)
+boxplot(crownvolume ~ volume, data=alpine_shape, las=2)
+
