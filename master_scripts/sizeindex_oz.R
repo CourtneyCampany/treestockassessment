7eb4b4c47049667fa461a76494a9b7e1fcd0331b
Courtney Campany <courtneycampany@gmail.com>
2016-12-13 18:08:49 +1100

    edits to specie names on raw files

--- master_scripts/sizeindex_oz.R
+++ master_scripts/sizeindex_oz.R
@@ -99,6 +99,8 @@ count(oz_sizeindex, var="balanced")
 
 
 #size index plotting--------------------------------------------------------
+oz_si_agg <- summaryBy(logSI ~ species + nursery+volume, 
+                       data=oz_sizeindex2, FUN=mean, keep.names = TRUE)
 
 library(magicaxis)
 library(RColorBrewer)
@@ -113,11 +115,11 @@ silab <- expression(Size~index~range~~(calliper~x~height))
 cols <-  brewer.pal(6, "Set1")
 cols2 <- c(alpha(cols[5], .65), alpha(cols[3], .65), alpha(cols[2], .65), alpha(cols[1], .65))
 
-windows(7,7)
-# png(filename = "master_scripts/ozsizeindex.png", width = 5, height = 5, units = "in", res= 600)
+# windows(7,7)
+png(filename = "master_scripts/ozsizeindex_batch_means.png", width = 5, height = 5, units = "in", res= 600)
 par(mar=c(5,5,2,1),cex.axis=1, cex.lab=1.25,las=0,mgp=c(3,1,0))
-plot(logSI ~ logvol, data=oz_sizeindex, xlab="Container volume (L)", ylab=silab,   xlim=c(0.5,3.8),ylim=c(0.3,3.5),
-     axes=FALSE, cex=1.25, bg=cols2[oz_sizeindex$climate_region],pch=21)
+plot(logSI ~ log10(volume), data=oz_si_agg, xlab="Container volume (L)", ylab=silab,   xlim=c(0.5,3.8),ylim=c(0.3,3.5),
+     axes=FALSE, cex=1.25, bg="olivedrab3",pch=21)
 
 magicaxis::magaxis(side=c(1,2), unlog=c(1,2), frame.plot=FALSE)
 
@@ -126,9 +128,9 @@ lines(log10(min_size_index[1:36])~log10(container_volume[1:36]), data=standard,l
 lines(log10(max_size_index[1:36])~log10(container_volume[1:36]), data=standard,lwd=2.5)
 
 legend("topleft", "AS2303 Size Index Range" ,lty=1, lwd=2,bty='n', inset=.01)
-legend("bottomright", c("New South Wales", "Northern Territory", "Perth", "Victoria"), pch=21, pt.bg =cols2, inset=.025)
-# title(main="Australia Tree Nurseries")
+#legend("bottomright", c("New South Wales", "Northern Territory", "Perth", "Victoria"), pch=21, pt.bg =cols2, inset=.025)
+title(main="Australia Tree Nurseries")
 box()
 
-#dev.copy2pdf(file="master_scripts/oz_sizeindex.pdf")
-# dev.off()
+# dev.copy2pdf(file="master_scripts/oz_sizeindex_batch_means.pdf")
+dev.off()
